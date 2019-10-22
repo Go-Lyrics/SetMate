9cc9b84de7e404f56c08aa91ad85c2d529f100af
Marlon <marmoneymar@icloud.com>
2019-10-22 03:08:58 -0400

    Bulk of CoreData done as well as model

---
+++ SetMate/SetMate/ModelControllers/FileController.swift
@@ -0,0 +1,15 @@
+//
+//  FileController.swift
+//  SetMate
+//
+//  Created by Marlon Raskin on 10/22/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import Foundation
+
+class FileController {
+
+	let fm = FileManager.default
+	
+}
---
+++ SetMate/SetMate/Models/SongFile.swift
@@ -0,0 +1,19 @@
+//
+//  SongFile.swift
+//  SetMate
+//
+//  Created by Marlon Raskin on 10/22/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension SongFile {
+	@discardableResult convenience init(name: String, filePath: String, id: UUID = UUID(), context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+		self.init(context: context)
+		self.name = name
+		self.filePath = filePath
+		self.id = id
+	}
+}
